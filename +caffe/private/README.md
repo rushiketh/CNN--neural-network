97c7dc57079c6b3aeac8492e2b7d65472b2c08e8
rushiketh <40553143+rushiketh@users.noreply.github.com>
2019-11-24 19:33:04 -0500

    Add files via upload

---
+++ +caffe/private/README.md
@@ -0,0 +1,18 @@
+# CaffeBuild
+
+## Env:
+OS: Windows
+
+
+Compiler: Visual Studio 2013
+
+
+Python: Anaconda2
+
+
+Matlab: 2015a
+
+
+Dependency: with CUDA & CUDNN
+
+
---
+++ +caffe/reset_all.m
@@ -0,0 +1,8 @@
+function reset_all()
+% reset_all()
+%   clear all solvers and stand-alone nets and reset Caffe to initial status
+ addpath('+caffe/private/caffe_.mexw64')
+caffe_('reset');
+is_valid_handle('get_new_init_key');
+
+end
